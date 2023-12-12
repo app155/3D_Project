93a3eadacd7541e46343eeb02b20bc1fd8347b56
FanTaSy <147117020+BisuMini@users.noreply.github.com>
2023-12-12 23:15:47 +0900

    stat

---
+++ Assets/02.Scripts/Controller/Stat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    public int level;
+    public int maxHp;
+    public int attack;
+    public int totalExp;
+  
+}
