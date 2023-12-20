fb93a6efbf29b9dd1e57d460c768f73bbdc6c03b
BisuMini <g131312@naver.com>
2023-12-20 15:56:58 +0900

    Update HPUI.cs

--- Assets/02.Scripts/Controller/UI/HPUI.cs
+++ Assets/02.Scripts/Controller/UI/HPUI.cs
@@ -6,25 +6,27 @@ using Project3D.Controller;
 using UnityEngine.UI;
 using Unity.Netcode;
 using System;
+using TMPro;
 
 namespace Project3D.UI
 {
     public class HPUI : NetworkBehaviour
     {
         [SerializeField] private Slider _hpBar;
-        [SerializeField] private Slider _Lv;
+        [SerializeField] public TMP_Text _Lv;
+
         private void Start()
         {
             IHp hp = transform.root.GetComponent<IHp>();
             _hpBar.minValue = hp.HpMin;
             _hpBar.maxValue = hp.HpMax;
             _hpBar.value = hp.HpValue;
-            _Lv.value = hp.Lv;
+            _Lv.text = _Lv.ToString();
 
-            hp.onLvChanged += (value) => _Lv.value = value;
+
             hp.onHpChanged += (value) => _hpBar.value = value;
 
-            // is 키워드 
+            // is 키워드 r55e
             // 객체가 어떤 타입으로 참조할 수 있는지 확인하고 bool 결과를 반환하는 키워드
             if (hp is CharacterControllers)
             {
@@ -37,7 +39,7 @@ namespace Project3D.UI
                 };
             }
         }
-     
+
     }
 }
 
